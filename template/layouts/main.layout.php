4fd7e56792e7ff0af5df8e0e19b3dfb77cd46b70
yoyi-88 <ygomben2510@g.educaand.es>
2026-05-14 20:06:08 +0200

    Logo en ventana de navegador

--- template/layouts/main.layout.php
+++ template/layouts/main.layout.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="<?= URL ?>public/images/logo.png">
     <title><?= $this->title ?? 'Chef Privado' ?></title>
 
     <link rel="stylesheet" href="<?= URL ?>public/css/main.css">
